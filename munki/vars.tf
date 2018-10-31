dcafb0737c9f87d115d29c3b61816830b2050e1d
Graham Gilbert <graham.gilbert@airbnb.com>
2018-10-31 16:01:10 -0700

    Defaults

--- munki/vars.tf
+++ munki/vars.tf
@@ -1,4 +1,13 @@
-variable "username" {}
-variable "password" {}
-variable "munki_s3_bucket" {}
-variable "price_class" {}
+variable "username" {
+    description = "Username clients use."
+}
+variable "password" {
+    description = "Password clients use."
+}
+variable "munki_s3_bucket" {
+    description = "The name of your s3 Bucket"
+}
+variable "price_class" {
+    default = "PriceClass_100"
+    description = "The price class your CloudFront Distribution should use."
+}
