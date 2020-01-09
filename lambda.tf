a53b8b7edd52dc7789e512ea4735e936d6e10259
Graham Gilbert <graham.gilbert@airbnb.com>
2020-01-09 08:44:06 -0800

    Update to node 10

--- lambda.tf
+++ lambda.tf
@@ -45,7 +45,7 @@ resource "aws_lambda_function" "basic_auth_lambda" {
   role             = "${aws_iam_role.iam_for_lambda.arn}"
   handler          = "basic_auth.handler"
   source_code_hash = "${data.archive_file.basic_auth_lambda_zip.output_base64sha256}"
-  runtime          = "nodejs8.10"
+  runtime          = "nodejs10.x"
 
   publish = true
 }
