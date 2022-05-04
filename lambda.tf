2fbfb600c45b2d773f47066b9a64bc344e54a17b
Justin Holt <4149218+jholt13@users.noreply.github.com>
2022-05-04 09:23:00 -0700

    Migrating to templatfile for new versions of terraform

--- lambda.tf
+++ lambda.tf
@@ -21,20 +21,18 @@ resource "aws_iam_role" "iam_for_lambda" {
 EOF
 }
 
-data "template_file" "basic_auth_js" {
-  template = file("${path.module}/basic_auth.js.tpl")
-
-  vars = {
+locals {
+  basic_auth_js = templatefile("${path.module}/basic_auth.js.tpl", {
     username = var.username
     password = var.password
-  }
+  })
 }
 
 data "archive_file" "basic_auth_lambda_zip" {
   type = "zip"
 
   output_path             = "basic_auth_lambda.zip"
-  source_content          = data.template_file.basic_auth_js.rendered
+  source_content          = local.basic_auth_js
   source_content_filename = "basic_auth.js"
 }
 
