d8739d57b4d63a7458d016ea2b122480daaaf8c6
Austin Culter <austin.culter@faire.com>
2023-04-26 11:53:24 +0200

    Fix typo where aws_s3_bucket_acl.log_bucket_acl.bucket pointed to wrong bucket

--- s3.tf
+++ s3.tf
@@ -56,7 +56,7 @@ resource "aws_s3_bucket" "log_bucket" {
 }
 
 resource "aws_s3_bucket_acl" "log_bucket_acl" {
-  bucket = aws_s3_bucket.www.id
+  bucket = aws_s3_bucket.log_bucket.id
   acl    = "log-delivery-write"
 }
 
